0b7e6e6fdc9267e59536522e1e25333b648ba293
yashghode11 <169008102+yashghode11@users.noreply.github.com>
2026-05-24 16:00:58 +0530

    Added 300 realistic listings to Wanderlust 2

--- app.js
+++ app.js
@@ -129,7 +129,9 @@ app.use((req,res,next)=>{
 // })
 
 
-
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter );
 app.use("/",userRouter);
